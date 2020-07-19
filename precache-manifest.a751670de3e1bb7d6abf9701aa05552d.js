37b91ad89e4626188ecc4c3a3b87a67275b5cc7a
Binod Nagarkoti <Binod1365@gmail.com>
2020-07-19 12:13:34 +0545

    Updates

---
+++ precache-manifest.a751670de3e1bb7d6abf9701aa05552d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5018525f897e579a15a19cc63bd12e2",
+    "url": "/tictactoe-react/index.html"
+  },
+  {
+    "revision": "9ee381d4eb6d06348adf",
+    "url": "/tictactoe-react/static/css/main.3fefda7a.chunk.css"
+  },
+  {
+    "revision": "3dbdcfd25fa390b67422",
+    "url": "/tictactoe-react/static/js/2.1b779f61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe-react/static/js/2.1b779f61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ee381d4eb6d06348adf",
+    "url": "/tictactoe-react/static/js/main.8dd1b470.chunk.js"
+  },
+  {
+    "revision": "269e021b2e42b50a7bb4",
+    "url": "/tictactoe-react/static/js/runtime-main.b4063603.js"
+  }
+]);
